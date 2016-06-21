6140ba38eae5e768700e6a9f14ddc21de1272b71
Axel Hecht <axel@pike.org>
2016-06-21 18:11:26 +0200

    refactor a bit

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', onload, true);
 
 function ondata(e) {
     console.log('CE', e.detail.newbugs);
-    bcl && bcl.renderBugs(e.detail.newbugs);
+    components && components.renderBugs(e.detail.newbugs);
 }
 
-class BugCount {
+class Component {
     constructor(prod, component, mount) {
         this.mount = mount;
-        this._dom = document.querySelector('#templates > .bugcount').cloneNode(true);
+        this._dom = document.querySelector('#templates > .prodcomp').cloneNode(true);
         this._dom.querySelector(".product").textContent = prod;
         this._dom.querySelector(".component").textContent = component;
         this._dom.id = (prod + '_' + component).replace(/\W/g,'_');
@@ -21,7 +21,7 @@ class BugCount {
         this._dom.querySelector(".panel-body").id = body_id;
         this._dom.querySelector("a").href = '#' + body_id;
         this.count_dom = this._dom.querySelector('.count');
-        this.count = 1;
+        this.count = 0;
         var child = null;
         for (var i=0, ii=mount.children.length; i<ii; ++i) {
             if (mount.children[i].id > this._dom.id) {
@@ -30,6 +30,9 @@ class BugCount {
             }
         }
         mount.insertBefore(this._dom, child);
+    }
+    add(bug) {
+        this.count += 1;
     }
     get count() {
         return this._count ? this._count : 0;
@@ -48,30 +51,29 @@ class BugCountList {
         this.mount = mount;
         this.children = new Map();
     }
+    get(bug) {
+        var key = bug.product + '_' + bug.component;
+        var component = this.children.get(key);
+        if (!component) {
+            component = new Component(bug.product, bug.component, this.mount);
+            this.children.set(key, component);
+        }
+        return component;
+    }
     renderBugs(newbugs) {
         if (newbugs.length == 0) return;
-        var updates = new Set();
         newbugs.forEach(function(bugid) {
             var bug = knownbugs.get(bugid);
-            var key = bug.product + '_' + bug.component;
-            updates.add(key);
-            if (this.children.has(key)) {
-                this.children.get(key).count += 1;
-            }
-            else {
-                this.children.set(key, new BugCount(bug.product, bug.component, this.mount));
-            }
+            var component = this.get(bug);
+            component.add(bug);
         }, this);
-        updates = Array.from(updates);
-        updates.sort();
-        
     }
 }
 
-var bcl;
+var components;
 function onload() {
-    bcl = new BugCountList(document.getElementById('app'));
+    components = new BugCountList(document.getElementById('app'));
     if (knownbugs.size) {
-        bcl.renderBugs(Array.from(knownbugs.keys()));
+        components.renderBugs(Array.from(knownbugs.keys()));
     }
 }
