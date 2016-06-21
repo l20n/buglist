468ad0ffc63f0f52bf2ef54278ff9f25077e37e0
Axel Hecht <axel@pike.org>
2016-06-21 14:04:28 +0200

    starting to track things

---
+++ app.js
@@ -0,0 +1,73 @@
+/* global knownbugs */
+"use strict";
+
+document.addEventListener('bug-data', ondata, true);
+document.addEventListener('DOMContentLoaded', onload, true);
+
+function ondata(e) {
+    console.log('CE', e.detail.newbugs);
+    bcl && bcl.renderBugs(e.detail.newbugs);
+}
+
+class BugCount {
+    constructor(prod, component, mount) {
+        this.mount = mount;
+        this._dom = document.querySelector('#templates > .bugcount').cloneNode(true);
+        this._dom.querySelector(".product").textContent = prod;
+        this._dom.querySelector(".component").textContent = component;
+        this._dom.id = prod + '\0' + component;
+        this.count_dom = this._dom.querySelector('.count');
+        this.count = 1;
+        var child = null;
+        for (var i=0, ii=mount.children.length; i<ii; ++i) {
+            if (mount.children[i].id > this._dom.id) {
+                child = mount.children[i];
+                break;
+            }
+        }
+        mount.insertBefore(this._dom, child);
+    }
+    get count() {
+        return this._count ? this._count : 0;
+    }
+    set count(val) {
+        this._count = val;
+        if (this.count_dom) {
+            this.count_dom.textContent = val;
+        }
+    }
+    
+}
+
+class BugCountList {
+    constructor(mount) {
+        this.mount = mount;
+        this.children = new Map();
+    }
+    renderBugs(newbugs) {
+        if (newbugs.length == 0) return;
+        var updates = new Set();
+        newbugs.forEach(function(bugid) {
+            var bug = knownbugs.get(bugid);
+            var key = bug.product + '\0' + bug.component;
+            updates.add(key);
+            if (this.children.has(key)) {
+                this.children.get(key).count += 1;
+            }
+            else {
+                this.children.set(key, new BugCount(bug.product, bug.component, this.mount));
+            }
+        }, this);
+        updates = Array.from(updates);
+        updates.sort();
+        
+    }
+}
+
+var bcl;
+function onload() {
+    bcl = new BugCountList(document.getElementById('app'));
+    if (knownbugs.size) {
+        bcl.renderBugs(Array.from(knownbugs.keys()));
+    }
+}
---
+++ buglist.js
@@ -0,0 +1,70 @@
+/* global URL, fetch, CustomEvent */
+
+var newbugs = new Set(), knownbugs = new Map();
+
+const bugapi = 'https://bugzilla.mozilla.org/rest/bug';
+const fields = 'id,product,component,summary'
+
+function getBugAPI() {
+    var url = new URL(bugapi);
+    url.searchParams.append('include_fields', fields);
+    return url;
+}
+
+function findGeckoL20n() {
+    var url = getBugAPI();
+    url.searchParams.append('alias', 'gecko-l20n');
+    fetch(url).then(function (response) {
+        console.log('got response');
+        return response.json();
+    }).then(saveBugs);
+}
+
+function saveBugs(data) {
+    console.log('got bugs');
+    console.log(data.bugs.length);
+    if (! data.bugs.length) {
+        // notify
+        console.log('done loading');
+        return;
+    }
+    newbugs = new Set();
+    data.bugs.forEach(function(bug) {
+        if (knownbugs.has(bug.id)) {
+            return;
+        }
+        newbugs.add(bug.id);
+        knownbugs.set(bug.id, bug);
+    });
+    if (newbugs.size) {
+        document.dispatchEvent(
+            new CustomEvent('bug-data', {'detail': {'newbugs': Array.from(newbugs)}})
+        );
+        queryMoreBugs();
+    }
+    else {
+        // notify
+        console.log('done loading');
+    }
+}
+
+function queryMoreBugs() {
+    var url = getBugAPI();
+    var params = url.searchParams;
+    // exclude knownbugs
+    // params.append('bug_id_type', 'nowords');
+    // params.append('bug_id', Array.from(knownbugs.keys()).join(','));
+    // query for blocks
+    // params.append('j_top', 'OR'); < NOT NEEDED
+    ['blocked'].forEach(function(rel, i) {
+        params.append('f' + (i + 1), rel);
+        params.append('o' + (i + 1), 'anywords');
+        params.append('v' + (i + 1), Array.from(newbugs).join(','));
+    });
+    fetch(url).then(function(response) {
+        console.log('got dependencies');
+        return response.json();
+    }).then(saveBugs);
+}
+
+findGeckoL20n();
